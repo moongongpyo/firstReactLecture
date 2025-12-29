c6aaf437b7b496d77bbbb74aea87bee2458367d3
Ted <bflook08307@gmail.com>
2025-12-29 17:39:08 +0900

    useMemo 활용해서 최적화

--- src/components/project2/Dd.jsx
+++ src/components/project2/Dd.jsx
@@ -1,6 +1,6 @@
 import './Dd.css';
 import Rd from "./Rd.jsx";
-import {useReducer} from "react";
+import {useMemo, useReducer} from "react";
 
 function reducer(state, action) {
   switch (action.type) {
@@ -20,11 +20,26 @@ const Dd = ({todos, onUpdate, onDelete}) => {
       data : e.target.value,
     })
   }
+
+
+  const {totalCount,doneCount,notDoneCount} =  useMemo(() => {console.log("getAnalyzedData 호출")
+      const totalCount = todos.length;
+      const doneCount = todos.filter((todo) => todo.isDone).length;
+      const notDoneCount = totalCount - doneCount;
+
+      return{totalCount,doneCount,notDoneCount};}, [todos]);
+    //의존성배열 : deps
+    //deps가 비어있으면 최초에만 랜더링 됨
   return <>
     <div className='Dd'>
       <h4>
         Todo List 🌱
       </h4>
+      <div>
+        <div>total: {totalCount}</div>
+        <div>done: {doneCount}</div>
+        <div>notDone: {notDoneCount}</div>
+      </div>
       <input onChange={onChange} placeholder='검색어를 입력하세요'/>
       {
         todos.filter((todo)=>todo.content.toLowerCase().includes(search))
